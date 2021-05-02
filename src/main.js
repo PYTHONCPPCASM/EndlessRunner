dcf89bc4a037209846693ab732008a3bd8dcc6c5
Chuanyu Xiao <cxiao9@ucsc.edu>
2021-05-01 21:48:16 -0700

    add title block

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+/*
+    Collabrator Chuanyu Xiao, Huang Can, Minghui Ye
+    game title : Mouse Runner
+    date completed : 05/01/2021
+    Creative Tilt : 
+
+        - the art style was appraised among the group member
+        
+        - I have implememented game in a way as if there were 5 invisible track on the
+            tile sprite, which is going to be easy for player to move the mouse and catch
+            the cheese/trap.
+*/
+
 let config = {
     type: Phaser.AUTO,
     width: 480,
